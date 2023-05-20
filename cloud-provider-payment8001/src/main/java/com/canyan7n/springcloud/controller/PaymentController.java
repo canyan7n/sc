aed2a2c5a17f6f74ae95380eeb8cba434c0cce02
canyan7n <canyann@163.com>
2023-05-20 18:54:08 +0800

    commit on idea in springcloud 订单模块构建，controller层,完成添加@RequestBody绑定请求路径的参数

--- cloud-provider-payment8001/src/main/java/com/canyan7n/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/canyan7n/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.canyan7n.springcloud.entities.Payment;
 import com.canyan7n.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author ：macheng
@@ -23,11 +20,11 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result;
         result = paymentService.create(payment);
         if (result > 0){
-            log.info("插入结果" + result + "xxxx");
+            log.info("插入结果" + result + payment);
             return new CommonResult(200,"插入成功",result);
         }else {
             return new CommonResult(444,"插入失败",null);
