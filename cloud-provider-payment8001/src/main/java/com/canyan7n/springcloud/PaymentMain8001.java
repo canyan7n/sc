b9810a929ab35f0a161d8034533a5b549c4588b1
canyan7n <canyann@163.com>
2023-05-20 15:57:54 +0800

    commit on idea in springcloud 支付模块构建，建module 改pom 写yml 主main

---
+++ cloud-provider-payment8001/src/main/java/com/canyan7n/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.canyan7n.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :PaymentMain8001
+ * @date ：2023/5/20 15:54
+ * @description：TODO
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
