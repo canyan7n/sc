bc21bb5a822f295d79d0bbf3e2a423f40a91a3fb
canyan7n <canyann@163.com>
2023-05-20 18:37:29 +0800

    commit on idea in springcloud 订单模块构建，controller层

--- cloud-consumer-order8002/src/main/java/com/canyan7n/springcloud/controller/OrderController.java
+++ cloud-consumer-order8002/src/main/java/com/canyan7n/springcloud/controller/OrderController.java
@@ -1,6 +1,11 @@
 package com.canyan7n.springcloud.controller;
 
+import com.canyan7n.springcloud.entities.CommonResult;
+import com.canyan7n.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +18,17 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class OrderController {
+    public static final String PAYMENT_URL = "http://localhost:8001";
     @Autowired
     private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Integer id){
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id,CommonResult.class);
+    }
 }
