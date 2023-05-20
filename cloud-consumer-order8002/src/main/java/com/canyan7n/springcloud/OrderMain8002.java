2019c910018c336ca77228a7e9668748f442ef68
canyan7n <canyann@163.com>
2023-05-20 18:03:52 +0800

    commit on idea in springcloud 订单模块构建，new 主程序

---
+++ cloud-consumer-order8002/src/main/java/com/canyan7n/springcloud/OrderMain8002.java
@@ -0,0 +1,18 @@
+package com.canyan7n.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :OrderMain8002
+ * @date ：2023/5/20 18:02
+ * @description：TODO
+ */
+@SpringBootApplication
+public class OrderMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain8002.class,args);
+    }
+}
