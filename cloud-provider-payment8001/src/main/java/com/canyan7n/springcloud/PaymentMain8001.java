eb5779223fb392b238d0423fec4b86f39aec5f25
canyan7n <canyann@163.com>
2023-05-23 16:06:57 +0800

    commit on idea in springcloud eureka构建，将client注册进去，改pom,yml,main

--- cloud-provider-payment8001/src/main/java/com/canyan7n/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/canyan7n/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.canyan7n.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ：macheng
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description：TODO
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
